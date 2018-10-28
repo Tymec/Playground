7634b84e9bd2aad459d7c6421dc0f6937fc968be
Tymec <tymek1rt@hotmail.com>
2018-10-28 16:46:30 +0100

    Update

--- freeImages.py
+++ freeImages.py
@@ -4,10 +4,16 @@ import random
 import uuid
 import time
 import pickle
+import json
 
 DELAY_TIME = 2.0
-URL = "http://kunde.reedfoto.no/bestilling/thumb/"
 STRING_SIZE = 8
+POSSIBILITIES = pow(26 + 10, STRING_SIZE)     #COUNT OF ALPHABET AND NUMBERS TO THE POWER OF STRING_SIZE
+URL = "http://kunde.reedfoto.no/bestilling/thumb/"
+PROXY = {
+    "https": "https://190.145.140.174:51010"
+}
+
 
 def load():
     with open("pickle/freeImages_not_working.pkl", 'rb') as pkl:
@@ -21,6 +27,8 @@ def save(merged_working, not_working):
         pickle.dump(not_working, pkl)
     with open("pickle/freeImages_working.pkl", 'wb') as pkl:
         pickle.dump(merged_working, pkl)
+    with open("pickle/freeImages_url.json", 'w') as f:
+        json.dump(merged_working, f, indent=4)
 
 def generate_random_string(base_string_character):
     ret_str = ''
@@ -44,7 +52,8 @@ def testImage(repeat_times=100):
             print("Processing code: {} - {} out of {} - {}".format(code, i, repeat, "Dupe"), end='\r')
             repeat += 1
             continue
-        response = requests.get(image_url).content
+            
+        response = requests.get(image_url, proxies=PROXY).content
         if not "Fatal" in str(response):
             works = True
             WORKING_current.append(image_url)
@@ -57,21 +66,23 @@ def testImage(repeat_times=100):
     print("Found {} valid images out of {} iterations - {:.3f}s".format(len(WORKING_current), i, end - start))
     merged_working = WORKING + WORKING_current
     save(merged_working, NOT_WORKING)
-    return WORKING, NOT_WORKING
+    return WORKING, NOT_WORKING, (end - start)
 
 if __name__ is __name__:
     while True:
         try:
-            WORKING, NOT_WORKING = testImage(200)
+            WORKING, NOT_WORKING, TIME = testImage(200)
             print((
                 "Current amount of codes that do not work: {}\n"
                 "Current amount of codes that work: {}\n"
                 "Estimated amount of codes left to process: {}\n"
+                "Estimated time left: {:.0f}h\n"
                 "---------------------------------------------------"
                 ).format(
                     len(NOT_WORKING), 
                     len(WORKING), 
-                    1000000 - (len(NOT_WORKING) + len(WORKING))
+                    POSSIBILITIES - (len(NOT_WORKING) + len(WORKING)),
+                    (round((POSSIBILITIES - (len(NOT_WORKING) + len(WORKING)))/200) * TIME)/pow(60, 2)
                 )
             )
             time.sleep(DELAY_TIME)
