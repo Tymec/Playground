52bb2b785c843c592fb2155cc3c62e29c1ceb37f
Tymec <tymek1rt@hotmail.com>
2019-03-12 11:45:17 +0100

    Update image2Sound.py

--- image2Sound.py
+++ image2Sound.py
@@ -1,15 +1,18 @@
 import argparse
-import pyaudio
+import ToneGenerator
 from PIL import Image
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', type=str, help="Image", dest="image", required=True)
 args = parser.parse_args()
 
-im = Image.open(args.image)
-pixels = im.load()
 
-for i in range(im.size[0]):
-    for j in range(im.size[1]):
-        print(pixels[i,j])
-
+def pixels_to_tones():
+    im = Image.open(args.image)
+    pixels = im.load()
+    
+    for i in range(im.size[0]):
+        for j in range(im.size[1]):
+            print(pixels[i,j])
+    
+    im.close()
