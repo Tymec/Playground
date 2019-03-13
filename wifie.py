58e48a69f52b1bee06fe64e72ed26d8129d113b8
Tymec <tymek1rt@hotmail.com>
2019-03-13 08:30:25 +0100

    Update wifie.py

--- wifie.py
+++ wifie.py
@@ -1,3 +1,10 @@
 import requests
 
 URL = "http://detectportal.firefox.com/success.txt"
+
+login_data = {
+  'user.username': 'xxx',
+  'user.password': 'xxx'
+}
+
+response = requests.post(URL, data=login_data)
