287739e016b1c28576ae3cb312943389d77b39a4
Tymoteusz Grabowski <tymek1rt@hotmail.com>
2019-06-02 00:14:01 +0200

    Update pTimeTracker.py

--- pTimeTracker.py
+++ pTimeTracker.py
@@ -4,6 +4,12 @@ import pickle
 import time
 import ujson
 import datetime
+import argparse
+
+
+parser = argparse.ArgumentParser(description='Process time tracker')
+parser.add_argument('-p', '--print', help="Print a process time", dest="print", metavar="print")
+args = vars(parser.parse_args())
 
 
 class Tracker:
@@ -21,7 +27,8 @@ class Tracker:
                 loaded_dict = pickle.load(f)
             return loaded_dict
         except FileNotFoundError:
-            return {}
+            _processes = Tracker.get_all_processes()
+            return _processes
 
     @staticmethod
     def get_all_processes():
@@ -36,19 +43,6 @@ class Tracker:
         return process_dict
 
     @staticmethod
-    def is_running(process_name):
-        """Check if process is still running"""
-        for process in psutil.process_iter():
-            try:
-                if process.name() == process_name:
-                    return True
-                # if process.is_running():
-                #     return True
-            except psutil.NoSuchProcess:
-                pass
-        return False
-
-    @staticmethod
     def compare_processes(main_dict, process_dict):
         """Compares two dicts containing processes"""
         main_dict_keys = main_dict.keys()
@@ -60,22 +54,28 @@ class Tracker:
             elif process_name not in main_dict_keys:
                 main_dict[process_name] = process_dict[process_name]
         return main_dict
-
+    
+    @staticmethod
+    def print_process_time(processes, process_name):
+        if not processes.get(process_name):
+            print(f"No process with the name {process_name} was not found.")
+            return
+        print(f"This process has a tracked time of {processes[process_name]['runtime'] / 60:.2} hours.")
 
 if __name__ == "__main__":
     processes = Tracker.load_dict()
-    # processes = Tracker.get_all_processes()
-    # Tracker.save_dict(processes)
-    try:
-        while True:
-            time.sleep(5)
-
-            start = time.time()
-            _processes = Tracker.get_all_processes()
-            print(f"Took {time.time() - start:.2}s")
-
-            processes = Tracker.compare_processes(processes, _processes)
-    except KeyboardInterrupt:
-        Tracker.save_dict(processes)
-        with open('pickle/processes.json', 'w') as f:
-            ujson.dump(processes, f, indent=4)
+    
+    if args['print']:
+        Tracker.print_process_time(processes, args['print'])
+    else:
+        try:
+            while True:
+                time.sleep(5)
+                start = time.time()
+                _processes = Tracker.get_all_processes()
+                print(f"Took {time.time() - start:.2}s")
+                processes = Tracker.compare_processes(processes, _processes)
+        except KeyboardInterrupt:
+            Tracker.save_dict(processes)
+            with open('pickle/processes.json', 'w') as f:
+                ujson.dump(processes, f, indent=4)
