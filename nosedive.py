633595cdafa73726f94e206c480f66510ddcea0f
Tymec <tymek1rt@hotmail.com>
2019-07-02 15:32:22 +0200

    Update nosedive.py

--- nosedive.py
+++ nosedive.py
@@ -4,6 +4,130 @@ import requests
 import time
 
 
+def create_profile(information):
+    '''Creates a profile'''
+    # Simple schema of the profile dictionary
+    profile = {
+        'data': {
+            'id': information['data']['id'],
+            'username': information['data']['username'],
+            'bio': information['data']['bio'],
+            'status': information['data']['status']
+        },
+        'rank': {
+            'current_rank': information['rank']['current_rank'],
+            'previous_ranks': [
+                information['rank']['previous_ranks'][0],
+                information['rank']['previous_ranks'][1],
+                information['rank']['previous_ranks'][2]
+            ]
+        },
+        'personal': {
+            'name': {
+                'first_name': information['personal']['name']['first_name'],
+                'middle_name': information['personal']['name']['middle_name'],
+                'last_name': information['personal']['name']['last_name']
+            },
+            'information': {
+                'birthday': information['personal']['information']['age'],
+                'gender': information['personal']['information']['gender'],
+                'height': information['personal']['information']['height'],
+                'weight': information['personal']['information']['weight'],
+                'relationship_status': information['personal']['information']['relationship_status'],
+            },
+            'contact': {
+                'phone_number': information['personal']['contact']['phone_number'],
+                'email': information['personal']['contact']['email']
+                'social_media': {
+                    'facebook': information['personal']['contact']['social_media']['facebook'],
+                    'snapchat': information['personal']['contact']['social_media']['snapchat'],
+                    'instagram': information['personal']['contact']['social_media']['instagram']
+                }
+            },
+            'address': {
+                'country': information['personal']['address']['country']
+                'city': information['personal']['address']['city'],
+                'zip_code': information['personal']['address']['zip_code'],
+                'street': information['personal']['address']['street'],
+            }
+        },
+        'images': {
+            'profile': information['images']['profile'],
+            'album': {
+                'image1': {
+                    'original': information['images']['album']['image1']['original'],
+                    'thumbnail': information['images']['album']['image1']['thumbnail'],
+                    'resized': {
+                        'small': information['images']['album']['image1']['resized']['small'],
+                        'medium': information['images']['album']['image1']['resized']['medium'],
+                        'large': information['images']['album']['image1']['resized']['large']
+                    }
+                },
+                'image2': {
+                    'original': information['images']['album']['image2']['original'],
+                    'thumbnail': information['images']['album']['image2']['thumbnail'],
+                    'resized': {
+                        'small': information['images']['album']['image2']['resized']['small'],
+                        'medium': information['images']['album']['image2']['resized']['medium'],
+                        'large': information['images']['album']['image2']['resized']['large']
+                    }
+                },
+                'image3': {
+                    'original': information['images']['album']['image3']['original'],
+                    'thumbnail': information['images']['album']['image3']['thumbnail'],
+                    'resized': {
+                        'small': information['images']['album']['image3']['resized']['small'],
+                        'medium': information['images']['album']['image3']['resized']['medium'],
+                        'large': information['images']['album']['image3']['resized']['large']
+                    }
+                }
+            }
+        },
+        'devices': {
+            'SM-G950FD': {
+                'hwid': information['devices']['SM-G950FD']['hwid'],
+                'date_added': information['devices']['SM-G950FD']['date_added'],
+                'os': information['devices']['SM-G950FD']['os'],
+                'ip': information['devices']['SM-G950FD']['ip']
+            },
+            'SM-455GFD': {
+                'hwid': information['devices']['SM-455GFD']['hwid'],
+                'date_added': information['devices']['SM-455GFD']['date_added'],
+                'os': information['devices']['SM-455GFD']['os'],
+                'ip': information['devices']['SM-455GFD']['ip']
+            }
+        },
+        'private': {
+            'tracking': {
+                'tracking_id': information['private']['tracking']['tracking_id']
+            },
+            'billing': {
+                'paypal': {
+                    'hashed': {
+                        'email': information['private']['billing']['paypal']['email'],
+                        'pasword': information['private']['billing']['paypal']['pasword']
+                    }
+                },
+                'credit_card': {
+                    'hashed': {
+                        'number': information['private']['billing']['paypal']['number'],
+                        'cvv': information['private']['billing']['paypal']['cvv'],
+                        'expiration_date': {
+                            'expiration_month': information['private']['billing']['paypal']['expiration_date']['expiration_month'],
+                            'expiration_year': information['private']['billing']['paypal']['expiration_date']['expiration_year']
+                        },
+                        'name': {
+                            'first_name': information['private']['billing']['paypal']['name']['first_name'],
+                            'last_name': information['private']['billing']['paypal']['name']['last_name']
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return profile
+
+
 def debug_random_profile_generator(final_rank, rating_amount, username=None, description=None, variation=5):
     '''Generates a random profile'''
     profile = {}
@@ -43,7 +167,6 @@ def rank(profile, profile_to_rank, rank):
         'rank': rank + profile['rank']
     }
     new_profile = calculate_rating(rank, profile_to_rank)
-    
     return new_profile
 
 
@@ -51,12 +174,12 @@ if __name__ == "__main__":
     time_start = time.time()
     
     my_profile = debug_random_profile_generator(3.156, 50, username='Tymec', description=';/', variation=5)
-    print(f'{my_profile["username"]}: {my_profile["rank"]} | {time.time() - time_start}')
+    print(f'{my_profile["username"]}: {my_profile["rank"]} | {time.time() - time_start}ms')
     
     time_start = time.time()
     another_profile = debug_random_profile_generator(4.755, 500, variation=5)
-    print(f'{another_profile["username"]}: {another_profile["rank"]} | {time.time() - time_start}')
+    print(f'{another_profile["username"]}: {another_profile["rank"]} | {time.time() - time_start}ms')
     
     time_start = time.time()
     my_profile = rank(another_profile, my_profile, 5.0)
-    print(f'{my_profile["username"]}: {my_profile["rank"]} | {time.time() - time_start}')
+    print(f'{my_profile["username"]}: {my_profile["rank"]} | {time.time() - time_start}ms')
